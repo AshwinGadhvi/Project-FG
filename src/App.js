34919a32039fd2674b39e80b4e4dc02a149642d2
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-07-21 18:34:37 +0530

    Adding Some Content.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
-import Register from './components/Register';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
+import Home from './pages/Home';
+import AdminDashboard from './pages/AdminDashboard';
+import StudentDashboard from './pages/StudentDashboard';
+import Attendance from './pages/Attendance';
 
-const App = () => {
-    return (
-        <Router>
-            <div>
-                <Switch>
-                    <Route path="/" exact component={Home} />
-                    <Route path="/register" component={Register} />
-                    <Route path="/login" component={Login} />
-                    <Route path="/dashboard" component={Dashboard} />
-                </Switch>
-            </div>
-        </Router>
-    );
-};
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/student-dashboard" element={<StudentDashboard />} />
+        <Route path="/attendance" element={<Attendance />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
