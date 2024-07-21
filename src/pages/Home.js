4ef67ddf17e4098124f01f706f099754500b0947
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-07-21 18:34:21 +0530

    Adding Some Content.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,16 @@
+
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div>
-            <h1>Welcome to the Attendance System</h1>
-            <Link to="/register">Register</Link>
-            <br />
-            <Link to="/login">Login</Link>
+            <h1>Home</h1>
+            <div>
+                <h1>Welcome to the Attendance System</h1>
+                <Link to="/register">Register</Link>
+                <br />
+                <Link to="/login">Login</Link>
+            </div>
         </div>
     );
 };
